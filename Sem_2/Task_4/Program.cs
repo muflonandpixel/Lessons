b84fc7cce4ca448d63c3558289cdf3a436c06e2e
muflon <mfedor722@gmail.com>
2023-12-27 13:45:33 +0300

    Sem_2:Task_4

---
+++ Sem_2/Task_4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = number/100;
+if (a==0)
+{
+    Console.WriteLine("нет");
+}
+else
+{
+    Console.WriteLine(a%10);
+}
