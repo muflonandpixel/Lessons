f084563d9313010deed6c30a575811d14f22a4b7
muflon <mfedor722@gmail.com>
2023-12-27 14:20:55 +0300

    homeworkToSem_2:Task_2

---
+++ homeworkToSem_2/Task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x==0||y==0)
+{
+    Console.WriteLine("Читайте условие задачи!");
+}
+else
+{
+    if (x>0 && y>0)
+    {
+        Console.WriteLine(1);
+    }
+    else if (x<0 && y<0)
+    {
+        Console.WriteLine(3);
+    }
+    else if (x>0 && y<0)
+    {
+        Console.WriteLine(4);
+    }
+    else
+    {
+        Console.WriteLine(2); 
+    }
+}
