667ad6c708bbd15d75ff53dd6a0ec1d6e3981728
muflon <mfedor722@gmail.com>
2023-12-27 13:19:54 +0300

    Sem_2:Task_1

---
+++ Sem_2/Task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000)
+{
+    int result = number/100*10+number%10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число!");
+}
