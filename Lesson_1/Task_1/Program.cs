6f4c7f737e815712408e56cdc246b7e8627b5991
muflon <mfedor722@gmail.com>
2023-12-27 11:02:22 +0300

    Папка

---
+++ Lesson_1/Task_1/Program.cs
@@ -0,0 +1,9 @@
+﻿//Console.WriteLine("Hello, World!");
+int value = 100;
+Console.WriteLine(value);
+double d = 6.763486767;
+char c = 'f';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
