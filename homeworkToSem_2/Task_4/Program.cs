caa0d81e8de6cee8302b39b54884153d30f36728
muflon <mfedor722@gmail.com>
2023-12-27 14:44:15 +0300

    homeworkToSem_2:Task_4

---
+++ homeworkToSem_2/Task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while ((int)number/i>9)
+{
+    i=i*10;
+}
+Console.Write((int)number/i);
+number = number % i;
+i=i/10;
+while (number>0)
+{
+    Console.Write(","+(int)number/i);
+    number = number % i;
+    i=i/10;
+}
