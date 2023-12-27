d7ac964c8a0d059a2916524a8bab00067f9ed6cb
muflon <mfedor722@gmail.com>
2023-12-27 14:31:45 +0300

    homeworkToSem_2:Task_3

---
+++ homeworkToSem_2/Task_3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number>=10 && number<=99)
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    if (firstDigit>secondDigit)
+    {
+        Console.WriteLine(firstDigit);
+    }
+    else
+    {
+        Console.WriteLine(secondDigit);
+    }
+}
+else
+{
+    Console.WriteLine("Читайте условие задачи!");
+}
