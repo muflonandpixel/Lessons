fd88138e407f9f4552d20dba205aac4ef1d773ed
muflon <mfedor722@gmail.com>
2023-12-27 10:39:21 +0300

    Переиенные

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("Hello, World!");
+int value = 100;
+Console.WriteLine(value);
+double d = 6.763486767;
+char c = 'f';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
